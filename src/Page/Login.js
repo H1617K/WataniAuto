a22749f2eefc62fca4d29881277941e2f57fe3de
harsh <harsh@WHOHARSH>
2024-01-03 11:00:37 +0530

    add En and dworn icon in login ja and css file

--- src/Page/Login.js
+++ src/Page/Login.js
@@ -3,52 +3,56 @@ import { img1, img2 } from '../Component/Images';
 import { FooterOption } from '../Component/Footer/Footer';
 import '../CSS/Login.css';
 import { Link } from 'react-router-dom';
+import { FaArrowDown } from "react-icons/fa";
 
 
 const Login = () => {
   return (
     <>
-    <section className="Main-login-Page">
-      <div className="back-img" >
-        <img src={img2} alt="img"/>
-        <section>
-          <div className="Main-Login-form">
-            <form>
-              <div className="Box-heder">
-                <h2>Welcome!</h2>
-              </div>
-              <div className='Box-Email'>
-                <input type="email" name="email" placeholder='Email*' required />
-              </div><br />
-              <div className='Box1-password'>
-                <input type="password" name="password" placeholder='Password*' required />
-              </div>
-              <div className="ForgotPassword">
-                <Link to="/Login">Forgot Password?</Link>
-              </div>
-              <br />
-              <div className='SubmitButton'>
-              <button type="submit">Login</button>
-              </div> <br />
-              <div className="NewUser">
-                 <h5>New User?</h5>
-                <div className='Register'>
-                  <button type='NewRegister'>Register</button>
+      <section className="Main-login-Page">
+        <div className="back-img" >
+          <img src={img2} alt="img"/>
+          <section>
+            <div className="Main-Login-form">
+              <form>
+                <div className="Box-heder">
+                  <h2>Welcome!</h2>
                 </div>
-              </div>
-            </form>          
-          </div>
-        </section>
-        <FooterOption/>
-      </div>
-      <section>
-          <div className='Header'>
-            <div className='Logo'>
-              <img src={img1} alt="Logo"/>
+                <div className='Box-Email'>
+                  <input type="email" name="email" placeholder='Email*' required />
+                </div><br />
+                <div className='Box1-password'>
+                  <input type="password" name="password" placeholder='Password*' required />
+                </div>
+                <div className="ForgotPassword">
+                  <Link to="/Login">Forgot Password?</Link>
+                </div>
+                <br />
+                <div className='SubmitButton'>
+                <button type="submit">Login</button>
+                </div> <br />
+                <div className="NewUser">
+                  <h5>New User?</h5>
+                  <div className='Register'>
+                    <button type='NewRegister'>Register</button>
+                  </div>
+                </div>
+              </form>          
             </div>
-          </div>
-        </section>
-    </section>
+          </section>
+          <FooterOption/>
+        </div>
+        <section>
+            <div className='Header'>
+              <div className='Logo'>
+                <img src={img1} alt="Logo"/>
+              </div>
+              <div className='EN'>
+                <h4>EN <FaArrowDown /></h4> 
+              </div>
+            </div>
+          </section>
+      </section>
     </>
   );
 }
