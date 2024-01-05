818b4b8f055daf8739b2cd16a668831504431486
harsh <harsh@WHOHARSH>
2024-01-05 11:41:06 +0530

    update Login js and css file

--- src/Page/Login.js
+++ src/Page/Login.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { img1, img2 } from '../Component/Images';
 import { FooterOption } from '../Component/Footer/Footer';
 import '../CSS/Login.css';
-import { Link } from 'react-router-dom';
-import { FaArrowDown } from "react-icons/fa";
+import { IoIosArrowDown } from "react-icons/io";
 
 
 const Login = () => {
@@ -19,13 +18,13 @@ const Login = () => {
                   <h2>Welcome!</h2>
                 </div>
                 <div className='Box-Email'>
-                  <input type="email" name="email" placeholder='Email*' required />
+                  <input type="email" name="email" placeholder=' E-mail*' required />
                 </div><br />
                 <div className='Box1-password'>
-                  <input type="password" name="password" placeholder='Password*' required />
+                  <input type="password" name="password" placeholder=' Password*' required />
                 </div>
                 <div className="ForgotPassword">
-                  <Link to="/Login">Forgot Password?</Link>
+                  <h6>Forget Password?</h6>
                 </div>
                 <br />
                 <div className='SubmitButton'>
@@ -34,7 +33,7 @@ const Login = () => {
                 <div className="NewUser">
                   <h5>New User?</h5>
                   <div className='Register'>
-                    <button type='NewRegister'>Register</button>
+                    <button type='NewRegister'>Register Now</button>
                   </div>
                 </div>
               </form>          
@@ -47,8 +46,20 @@ const Login = () => {
               <div className='Logo'>
                 <img src={img1} alt="Logo"/>
               </div>
-              <div className='EN'>
-                <h4>EN <FaArrowDown /></h4> 
+              <div className='language-container'>
+                <div className='lang-dropdown'>
+                  <button className='lang-dropdown-button'>
+                    <p>EN</p>
+                    <div className='arrow-vect'>
+                       <IoIosArrowDown />
+                    </div>
+                  </button>
+                    <div className='lang-dropdown-content'>
+                      <h6> English </h6>
+                      <h6> Hindi </h6>
+                      <h6> Gujarati </h6>
+                     </div>
+                </div>
               </div>
             </div>
           </section>
