1ef50d709743fcb9029aaa60e76dd2dc08a1904c
harsh <harsh@WHOHARSH>
2024-01-08 16:30:43 +0530

    update code for css and js for login

--- src/Page/Login.js
+++ src/Page/Login.js
@@ -2,43 +2,40 @@ import React, { useState } from 'react';
 import { img1, img2 } from '../Component/Images';
 import { FooterOption } from '../Component/Footer/Footer';
 import '../CSS/Login.css';
-import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowDown } from 'react-icons/io';
 import { useNavigate } from 'react-router-dom';
-// import { useFormValidation } from '../Utils/formValidation';
-
 
 const Login = () => {
-  // const [emailplaceholder, setEmailPlaceholder] = useState("Email*")
-  // // const [Passwordplaceholder,setPasswordPlaceholder] = useState("Password*")
-  // const [backgroundColor, setbackgroundColor] = useState('')
-  // const [isEmailFocused, setIsEmailFocused] = useState(false);
-  // const [isPasswordFocused, setIsPasswordFocused] = useState(false);
-  // const [showPassword, setShowPassword] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const NavigationType = useNavigate();
+  const navigate = useNavigate();
+  const [emailPlaceholder, setEmailPlaceholder] = useState('Email*');
+  const [passwordPlaceholder, setPasswordPlaceholder] = useState('Password*');
+  const [isEmailFocused, setIsEmailFocused] = useState(false);
+  const [isPasswordFocused, setIsPasswordFocused] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
-  // const togglePasswordVisibility = () => {
-  //   setShowPassword(!showPassword);
-  // };
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
-  // const onfocus = () => {
-  // setemailplaceholder("example@domain.com")
-  // setbackgroundColor('blue')
-  // setIsEmailFocused(true);
-  // }
-  // const onBlur = () => {
-  // setEmailPlaceholder("Email*")
-  // setIsEmailFocused(email !== '');
-  // }
+  const handleEmailFocus = () => {
+    setEmailPlaceholder('example@domain.com');
+    setIsEmailFocused(true);
+  };
 
-  // const OnPasswordFocus = () => {
-  // setIsPasswordFocused(true);
-  // }
+  const handleEmailBlur = () => {
+    setEmailPlaceholder('Email*');
+    setIsEmailFocused(email !== '');
+  };
 
-  // const onPasswordBlur = () => {
-  // setIsPasswordFocused(false);
-  // }
+  const handlePasswordFocus = () => {
+    setIsPasswordFocused(true);
+  };
+
+  const handlePasswordBlur = () => {
+    setIsPasswordFocused(false);
+  };
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -48,89 +45,106 @@ const Login = () => {
     setPassword(e.target.value);
   };
 
-  // const { email, password, handlerSubmit, emailError, passwordError, handleEmailChange, handlePasswordChange } = useFormValidation()
-
-    
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Store in local storage
     localStorage.setItem('User', JSON.stringify({ email, password }));
-
-    // Print in JSON format on the console
     console.log(JSON.stringify({ email, password }));
   };
 
-   // Navifgate for the regidter page for New Register
   const handleRegisterClick = () => {
-    NavigationType("/NewRegister");
-  }
+    navigate('/NewRegister');
+  };
+
+  const handleForgetPassword = () => {
+    navigate('/Forgetpassword');
+  };
 
   return (
-    <>
-      <section>
-       <div className="Main-login-Page">
-        <div className="back-img" >
-          <img src={img2} alt="img"/>
+    <section>
+      <div className="Main-login-Page">
+        <div className="back-img">
+          <img src={img2} alt="img" />
           <section>
             <div className="Main-Login-form">
               <form onSubmit={handleSubmit}>
                 <div className="Box-heder-Welcome">
                   <h2>Welcome!</h2>
                 </div>
-                <div className='Box-Email'>
-                {/* {isEmailFocused ?
-                  <label className={`lable_holder lable-email ${isEmailFocused ? 'focused' : ''}`}>Email*</label>
-                  : ""} */}
-                  <input type="email" name="email" value={email} onChange={handleEmailChange} required />
-                  <label className='Lable-Holder-Email'>Email*</label>
-                </div><br />
-                <div className='Box1-password'>
-                  <input type="password" name="password" value={password} onChange={handlePasswordChange}  required />
-                  <label className='Lable-Holder-Password'>Password*</label>
-                </div>
-                <div className="ForgotPassword">
-                  <h6>Forget Password?</h6>
+                <div className="Box-Email">
+                  {isEmailFocused && <label className="Label-Holder-Email">Email*</label>}
+                  <input
+                    type="email"
+                    name="email"
+                    value={email}
+                    placeholder={emailPlaceholder}
+                    onChange={handleEmailChange}
+                    onFocus={handleEmailFocus}
+                    onBlur={handleEmailBlur}
+                    required
+                  />
                 </div>
                 <br />
-                <div className='SubmitButton'>
-                <button type="submit">Login</button>
-                </div> <br />
-                <div className="NewUser">
-                  <h5>New User?</h5>
-                  <div className='Register'>
-                    <button type='button' onClick={handleRegisterClick}>Register Now</button>
+                <div className="Box1-password">
+                  {isPasswordFocused && <label className="Label-Holder-Password">Password*</label>}
+                  <input
+                    type={showPassword ? 'text' : 'password'}
+                    name="password"
+                    value={password}
+                    placeholder={isPasswordFocused ? '' : 'Password*'}
+                    onChange={handlePasswordChange}
+                    onFocus={handlePasswordFocus}
+                    onBlur={handlePasswordBlur}
+                    required
+                  />
+                </div>
+                <br />
+                <div className="ForgotPassword">
+                  <div className='OnclickForgetpassword'>
+                    <h6 onClick={handleForgetPassword}>Forget password?</h6>
                   </div>
                 </div>
-              </form>          
+                <br />
+                <div className="SubmitButton">
+                  <button type="submit">Login</button>
+                </div>
+                <br />
+                <div className="NewUser">
+                  <h5>New User?</h5>
+                  <div className="Register">
+                    <button type="button" onClick={handleRegisterClick}>
+                      Register Now
+                    </button>
+                  </div>
+                </div>
+              </form>
             </div>
           </section>
-          <FooterOption/>
+          <FooterOption />
         </div>
         <section>
-        <div className='Header'>
-              <div className='Logo'>
-                <img src={img1} alt="Logo"/>
-              </div>
-              <div className='language-container'>
-                <div className='lang-dropdown'>
-                  <button className='lang-dropdown-button'>
-                    <p>EN</p>
-                    <div className='arrow-vect'>
-                       <IoIosArrowDown />
-                    </div>
-                  </button>
-                    <div className='lang-dropdown-content'>
-                      <h6> English </h6>
-                      <h6> Arabic </h6>
-                     </div>
+          <div className="Header">
+            <div className="Logo">
+              <img src={img1} alt="Logo" />
+            </div>
+            <div className="language-container">
+              <div className="lang-dropdown">
+                <button className="lang-dropdown-button">
+                  <p>EN</p>
+                  <div className="arrow-vect">
+                    <IoIosArrowDown />
+                  </div>
+                </button>
+                <div className="lang-dropdown-content">
+                  <h6> English </h6>
+                  <h6> Arabic </h6>
                 </div>
               </div>
             </div>
-          </section>
-        </div>
-      </section>
-    </>
+          </div>
+        </section>
+      </div>
+    </section>
   );
-}
+};
 
 export default Login;
