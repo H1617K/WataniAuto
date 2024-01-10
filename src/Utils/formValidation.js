c6376f347144ce263e0f36a721b61cc2e63309ea
harsh <harsh@WHOHARSH>
2024-01-10 10:31:31 +0530

    Update Validation

--- src/Utils/formValidation.js
+++ src/Utils/formValidation.js
@@ -1,43 +1,60 @@
-import  { useState } from 'react'
+import { useState } from 'react'
 
-export const isValidEmail = (email)  =>{
-    const emailRegex =/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email)
-;    
+
+export const isValidEmail = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email)
+;
 }
 
-export const isValidPassword = (password) =>{
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    return passwordRegex.test(password);    
+export const isValidPassword = (password) => {
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  return passwordRegex.test(password);
 }
 
 export const useFormValidation = () => {
-    const [email,setEmail] = useState('')
-    const [password,setPassword] = useState("")
-    const [emailError,setEmailError] = useState("")
-    const [passwordError,setPasswordError] = useState("")
-    
-     const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-        setEmailError('');
-      };
-     const handlePasswordChange = (e) => {
-          setPassword(e.target.value);
-          setPasswordError('');
-        };
-    
-     const handlerSubmit= (e) =>{
-        e.preventDefault()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState("")
+  const [emailError, setEmailError] = useState("")
+  const [passwordError, setPasswordError] = useState("");
+  const [isEmailFocused, setIsEmailFocused] = useState(false)
+  const [isPasswordFocused, setIsPasswordFocused] = useState(false)
+  const [formData, setFormData] = useState([])
 
-        if(!isValidEmail(email)){
-            setEmailError("Please enter valid emailId")
-        }
-       if(!isValidPassword(password))
-            setPasswordError("please enter valid password")
-        return;
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    setEmailError('');
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    setPasswordError('');
+  };
+
+  const handlerSubmit = (e) => {
+    e.preventDefault()
+
+    if (!isValidEmail(email)) {
+      setEmailError("Please enter the Correct email id.")
     }
-  return {
-    email,password,handlerSubmit,emailError,passwordError,handleEmailChange,handlePasswordChange
+    if (!isValidPassword(password))
+      setPasswordError("please enter the Correct Password.")
+
+    if (isValidEmail(email)
+ && isValidPassword(password)) {
+
+      setEmail("")
+      setPassword("")
+      setIsEmailFocused(false)
+      setIsPasswordFocused(false)
+
+      const newData = { email, password }
+      setFormData((prevData) => [...prevData, newData])
+
+      console.log(`Email: ${newData.email}, Password: ${newData.password}`);
+
+    }
   }
-  
+  return {
+    email, password, setEmail, setPassword, isEmailFocused, setIsEmailFocused, isPasswordFocused, setIsPasswordFocused, handlerSubmit, emailError, passwordError, handleEmailChange, handlePasswordChange, formData
+  }
 }
