bda50f2d38861541c1283e458a7fa072bfce70e0
harsh <harsh@WHOHARSH>
2024-01-11 10:44:41 +0530

    Add New BackGround Image on Loginpage

--- src/Component/Images.js
+++ src/Component/Images.js
@@ -2,8 +2,9 @@ import img1 from '../Photos/Logo1.png';
 import img2 from '../Photos/changan_hunter_pu.jpg'
 import img3 from '../Photos/HomePage.png'
 import img4 from '../Photos/Rectangle 30.png'
+import img5 from '../Photos/changan_hunter_pu.png'
 
 export {
-    img1,img2,img3,img4
+    img1,img2,img3,img4,img5
 }
 
