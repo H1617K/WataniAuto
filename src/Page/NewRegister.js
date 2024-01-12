b88590d939e98c9db1730d496209e4b0e90f48bb
harsh <harsh@WHOHARSH>
2024-01-12 14:48:29 +0530

    Add Tooggle password and css anf change language English TO arabic and LTR TO RTL

--- src/Page/NewRegister.js
+++ src/Page/NewRegister.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { img1, img2 } from '../Component/Images';
+import React, { useState } from 'react';
+import { img1, img5,  } from '../Component/Images';
 import '../CSS/NewRegister.css';
 import { FooterOption } from '../Component/Footer/Footer';
 import { IoIosArrowDown } from 'react-icons/io';
@@ -7,15 +7,24 @@ import { FaArrowLeft, FaCircleInfo } from "react-icons/fa6";
 import { useTranslation } from 'react-i18next';
 import i18n from '../Utils/LanguageLocalization';
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
 
 const Register = () => {
   const { t } = useTranslation();
   const langdirection = i18n.language === "عربي" ? "rtl" : "ltr"; // Language Direction
+  const [showPassword, setShowPassword] = useState(false);  // Show Paaword
+  const [showconfimpassword, setShowconfimPassword] = useState(false);  // Show ConfimPaaword
 
   const changeLanguage = (newLanguage) => {
     i18n.changeLanguage(newLanguage);
   };
+
+  const toogglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const tooggleConfimPasswordVisibility = () => {
+    setShowconfimPassword(!showconfimpassword)
+  }
 
   // Move useNavigate outside of the handleToLogin function
   const navigate = useNavigate();
@@ -24,56 +33,58 @@ const Register = () => {
     navigate('/Login');
   };
 
-  // const handleToHome = () => {
-  //   navigate('/Home')
-  // }
-
   return (
     <>
       <section className="Main-Register-Page" dir={langdirection}>
         <div className="back-img">
-          <img src={img2} alt="img" />
+          <img src={img5} alt="img" />
           <section>
             <form>
               <div className='Main-Register-form'>
-                 <div className='Back-To-Login'>
-                   <button type='button' onClick={handleToLogin}><FaArrowLeft /></button>
-                 </div>
-                 <div className="Box-heder1">
-                   <h2>{t("Create an Account")}</h2>
-                 </div><br />
-                 <div className='Box1'>
-                    <div className='FristName'>
-                        <input type='text' placeholder='Frist Name*' required />
-                    </div>
-                    <div className='LastName'>
-                        <input type='text' placeholder='Last Name*' required />
-                    </div>
-                 </div><br />
-                 <div className='Box2'>
-                    <div className='Email'>
-                        <input type='Email' placeholder='Email*' required />
-                    </div>
-                    <div className='Info'>
+                <div className='Back-To-Login'>
+                  <button type='button' onClick={handleToLogin}><FaArrowLeft /></button>
+                </div>
+                <div className="Box-heder1">
+                  <h2>{t("CreateanAccount")}</h2>
+                </div><br />
+                <div className='Box1'>
+                  <div className='FristName'>
+                    <input type='text' placeholder={t("FirstName")} required />
+                  </div>
+                  <div className='LastName'>
+                    <input type='text' placeholder={t("LastName")} required />
+                  </div>
+                </div><br />
+                <div className='Box2'>
+                  <div className='Email'>
+                    <input type='Email' placeholder={t("Email")} required />
+                  </div>
+                  <div className='Info'>
                     <FaCircleInfo />
-                    </div>
+                  </div>
                  </div><br />
                   <div className='Box3'>
-                     <div className='CountyCode'>
-                      <input type='positivenumber' placeholder='+966' required/>
-                     </div>
-                     <div className='MobileNumber'>
-                      <input type='positivenumber' placeholder='Mobile Number*' required/>
-                     </div>
+                    <div className='CountyCode'>
+                      <input type='positivenumber' placeholder={t("Number")} required/>
+                    </div>
+                    <div className='MobileNumber'>
+                     <input type='positivenumber' placeholder={t("MobileNumber")} required/>
+                    </div>
                   </div><br />
                  <div className='Box4'>
                     <div className='Password'>
-                        <input type='Password' placeholder='Password*' required />
+                      <input type={showPassword ? 'text' : 'password'} placeholder={t("Password")} required />
+                      <button type='button' className='tooggle-password1' onClick={toogglePasswordVisibility} >
+                        {showPassword ? t("Hide") : t("Show")}
+                      </button>
                     </div>
                  </div><br />
                  <div className='Box5'>
                     <div className='Confrim-Password'>
-                        <input type='Password' placeholder='Confrim Password*' required />
+                      <input type={showconfimpassword ? 'text' : 'password'} placeholder={t("ConfrimPassword")} required />
+                      <button type='button' className='tooggle-password-confim' onClick={tooggleConfimPasswordVisibility} >
+                        {showconfimpassword ? t("Hide") : t("Show")}
+                      </button>
                     </div>
                  </div><br />
                  <div className='Box6'>
@@ -81,13 +92,13 @@ const Register = () => {
                    <input type="checkbox" id="transparentCheckbox" className="transparent-checkbox" required/>
                   </div>
                   <div className='Notice'>
-                    <h6>By creating a watani account, I agree to the  Terms of Use, and Privacy Notice.</h6>
+                    <h6>{t("Notice")}</h6>
                   </div>
                  </div>
-                    <div className='SubmitButton'>
-                     <button type='submit'>Create Account</button>
-                    </div>
-                    </div>
+                  <div className='SubmitButton'>
+                    <button type='submit'>{t("CreateAccount")}</button>
+                  </div>
+            </div>
             </form>
           </section>
           <FooterOption />
