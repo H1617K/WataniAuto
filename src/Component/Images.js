477deaa4fb32d2e1166239576e65b2888840510e
harsh <harsh@WHOHARSH>
2024-02-05 12:19:31 +0530

    add image for requied cars

--- src/Component/Images.js
+++ src/Component/Images.js
@@ -3,7 +3,17 @@ import img2 from '../Photos/changan_hunter_pu.jpg'
 import img3 from '../Photos/HomePage.png'
 import img4 from '../Photos/Rectangle 30.png'
 import img5 from '../Photos/changan_hunter_pu.png'
+import HomeImg from '../Photos/HomePage.png'
+import Hyundai from '../Photos/Hyundai.png'
+import Isuzu from   '../Photos/Isuzu.png'
+import MG from '../Photos/MG.png'
+import GAC from '../Photos/GAC.png'
+import Kia from '../Photos/Kia.png'
+import Changan from '../Photos/Changan.png'
+import Maruti from '../Photos/Maruti.png'
+import Ycompany from '../Photos/Ycompany.png'
+import Changancar from '../Photos/ChanganCar.png'
 
 export {
- img1,img2,img3,img4,img5
+ img1,img2,img3,img4,img5,HomeImg,Hyundai,Isuzu,MG,GAC,Kia,Changan,Maruti,Ycompany,Changancar
 }
