0ae2071bfabafb55a592995524671df1d32d396d
harsh <harsh@WHOHARSH>
2024-02-15 09:26:34 +0530

    Add Hmoe hareder

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Page/Home';
 import Login from './Page/Login';
 import NewRegister from './Page/NewRegister';
 import Forgetpassword from './Page/Forgetpassword';
+import  Home from './Page/Home';
 
 function App() {
   return (
     <>
     <BrowserRouter>
       <Routes>
-        <Route path="Home" element={<Home />} />
+        <Route path="Home" element={<Home/>} />
       </Routes>
       <Routes>
         <Route path="Login" element={<Login/>} />
